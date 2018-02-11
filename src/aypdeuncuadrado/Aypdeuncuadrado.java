2d6e7e7b9512ec46dc9563cedf635b64dba7ac85
iMrJulian <juliandrincon@gmail.com>
2018-02-10 19:49:22 -0500

    Area y perimetro
    Co-Authored-By: leonardowiesner11 <leonardowiesner11@users.noreply.github.com>
    Co-Authored-By: DavidBBBB <davidbbbb@users.noreply.github.com>

--- src/aypdeuncuadrado/Aypdeuncuadrado.java
+++ src/aypdeuncuadrado/Aypdeuncuadrado.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package aypdeuncuadrado;
-
+import java.util.Scanner;
 /**
  *
  * @author david
@@ -15,6 +15,14 @@ public class Aypdeuncuadrado {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        int l,a,p;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escriba el lado del cuadrado");
+        l = sc.nextInt();
+        a = l*l;
+        p = l*4;
+        System.out.println("El area del cuadrado es: "+a);
+        System.out.println("El perimetro del cuadrado es: "+p);
         // TODO code application logic here
     }
     
