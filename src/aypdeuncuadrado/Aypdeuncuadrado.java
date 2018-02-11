8cc99238be0b0a7805d9a851a59dc5ba9780f503
iMrJulian <juliandrincon@gmail.com>
2018-02-10 20:28:42 -0500

    Final
    Co-Authored-By: leonardowiesner11 <leonardowiesner11@users.noreply.github.com>
    Co-Authored-By: DavidBBBB <davidbbbb@users.noreply.github.com>

--- src/aypdeuncuadrado/Aypdeuncuadrado.java
+++ src/aypdeuncuadrado/Aypdeuncuadrado.java
@@ -1,3 +1,4 @@
+// Julian David Rincon, Leonardo Wiesner, Juan David Blanco
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
